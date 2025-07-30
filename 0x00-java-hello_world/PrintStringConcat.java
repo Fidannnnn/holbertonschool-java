95215a8fe2ce876bddd42cd4a53180314acf3a10
Fidan Alizada <fidanalizada06@gmail.com>
2025-07-30 16:25:56 +0000

    .

---
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -0,0 +1,8 @@
+public class PrintStringConcat {
+    String text1 = "Holberton";
+    String text2 = "School";
+    String text3 = text1 + " " + text2;
+    System.out.print("Welcome to ");
+    System.out.print(text3);
+    System.out.print(" !");
+}
