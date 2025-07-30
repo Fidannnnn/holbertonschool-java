58f438473d71637c56251a87bc725b943eb91f4b
Fidan Alizada <fidanalizada06@gmail.com>
2025-07-30 16:07:10 +0000

    .

--- 0x00-java-hello_world/PrintFloatVariable.java
+++ 0x00-java-hello_world/PrintFloatVariable.java
@@ -1,6 +1,6 @@
 public class PrintFloatVariable {
     public static void main(String[] args) {
         float pi = (float) 3.14159;
-        System.out.printf("Pi Value: %.3f", pi);
+        System.out.printf("Pi Value: %.3f ", pi);
     }
 }
