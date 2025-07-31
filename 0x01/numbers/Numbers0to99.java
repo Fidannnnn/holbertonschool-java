8ecb353d796b5bb4841c027f0ad035c0a771a048
Fidan Alizada <fidanalizada06@gmail.com>
2025-07-31 11:01:50 +0000

    .

--- 0x01/numbers/Numbers0to99.java
+++ 0x01/numbers/Numbers0to99.java
@@ -1,10 +1,11 @@
 public class Numbers0to99 {
-    public static void main(String args[]){
-        for(int i = 0; i < 100; i++){
-            if (i == 99){
-                System.out.printf("%d",i);
-            } 
-            else System.out.printf("%d, ",i);
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            if (i < 99) {
+                System.out.print(i + ", ");
+            } else {
+                System.out.println(i);
+            }
         }
     }
 }
