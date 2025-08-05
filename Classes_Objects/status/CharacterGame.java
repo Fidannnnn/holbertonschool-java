298c50b152df99225468e7cb54df03354b72c56b
FA <fidanalizada06@gmail.com>
2025-08-05 10:09:58 +0000

    .

---
+++ Classes_Objects/status/CharacterGame.java
@@ -0,0 +1,38 @@
+public class CharacterGame {
+    private int currentHealth;
+    private String name;
+    private String status;
+
+    public int getCurrentHealth() {
+        return currentHealth;
+    }
+
+    public void setCurrentHealth(int currentHealth) {
+        this.currentHealth = currentHealth;
+        if (currentHealth > 0) {
+            status = "alive";
+        } else {
+            status = "dead";
+        }
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void takeDamage(int damageAmount) {
+        setCurrentHealth(Math.max(currentHealth - damageAmount, 0));
+    }
+
+    public void receiveHealing(int healingAmount) {
+        setCurrentHealth(Math.min(currentHealth + healingAmount, 100));
+    }
+}
