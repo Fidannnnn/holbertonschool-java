a6e20afd66b06d1213590062556ca8eebdd8f53e
f <fidanalizada06@gmail.com>
2025-08-21 15:37:47 +0000

    .

--- List/todo/Task.java
+++ List/todo/Task.java
@@ -10,35 +10,18 @@ public class Task {
         this.isDone = false;
     }
 
-    // Getters
-    public String getDescription() {
-        return description;
-    }
+    public String getDescription() { return description; }
+    public boolean isDone() { return isDone; }
+    public int getIdentifier() { return identifier; }
 
-    public boolean isDone() {
-        return isDone;
-    }
-
-    public int getIdentifier() {
-        return identifier;
-    }
-
-    // Modify description with validation
     public void modifyDescription(String newDescription) {
         validateDescription(newDescription);
         this.description = newDescription;
     }
 
-    // Helpers to change status
-    public void markDone() {
-        this.isDone = true;
-    }
+    public void markDone() { this.isDone = true; }
+    public void undo() { this.isDone = false; }
 
-    public void undo() {
-        this.isDone = false;
-    }
-
-    // Validation helper
     private void validateDescription(String text) {
         if (text == null || text.trim().isEmpty()) {
             throw new IllegalArgumentException("Invalid task description");
