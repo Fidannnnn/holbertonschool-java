38ed4faa156e8c5c0d2949b73e69821fbaca18c2
f <fidanalizada06@gmail.com>
2025-08-26 06:20:41 +0000

    .

---
+++ Set_Map/duplicate_phones/Phone.java
@@ -0,0 +1,31 @@
+import java.util.Objects;
+
+public class Phone {
+    private String areaCode;
+    private String number;
+
+    public Phone(String areaCode, String number) {
+        this.areaCode = areaCode;
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + areaCode + ") " + number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Phone)) return false;
+        Phone phone = (Phone) o;
+        return Objects.equals(areaCode, phone.areaCode) &&
+               Objects.equals(number, phone.number);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(areaCode, number);
+    }
+}
+
