ceb0f4027504ed570c4c5de6e301b59a33acb55f
f <fidanalizada06@gmail.com>
2025-08-26 07:06:10 +0000

    .

--- Collections_Exercises/blog/Post.java
+++ Collections_Exercises/blog/Post.java
@@ -1,26 +1,46 @@
-public class Post {
-    private final String author;
+import java.util.Objects;
+
+public class Post implements Comparable<Post> {
+    private final Author author;
     private final String title;
     private final String body;
-    private final String category;
+    private final Categories category;
 
-    public Post(String author, String title, String body, String category) {
+    public Post(Author author, String title, String body, Categories category) {
         this.author = author;
         this.title = title;
         this.body = body;
         this.category = category;
     }
 
-    public String getAuthor() {
-        return author;
-    }
-    public String getTitle() {
+    public Author getAuthor() { return author; }
+    public String getTitle() { return title; }
+    public String getBody() { return body; }
+    public Categories getCategory() { return category; }
+
+    @Override
+    public String toString() {
         return title;
     }
-    public String getBody() {
-        return body;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Post)) return false;
+        Post p = (Post) o;
+        return Objects.equals(author, p.author) &&
+               Objects.equals(title, p.title);
     }
-    public String getCategory() {
-        return category;
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(author, title);
+    }
+
+    @Override
+    public int compareTo(Post other) {
+        int byTitle = this.title.compareTo(other.title);
+        if (byTitle != 0) return byTitle;
+        return this.author.toString().compareTo(other.author.toString());
     }
 }
--- Collections_Exercises/blog/Program.java
+++ Collections_Exercises/blog/Program.java
@@ -4,43 +4,88 @@ import java.util.*;
         public static void main(String[] args) {
             Blog blog1 = new Blog();
 
-            Post post1 = new Post("John Bannons", 
-                "How to implement Terraform",
+            Author author1 = new Author("John", "Bannons");
+            Author author2 = new Author("Jane", "Doe");
+            Author author3 = new Author("Peter", "Dirkly");
+
+            Post post1 = new Post(author1, 
+                "How to Implement Terraform",
                 "Implementing IaC with Terraform bla bla bla",
-                "DevOps");
+                Categories.DEVOPS);
 
-            Post post2 = new Post("John Bannons", 
-                "Working with the Set interface in Java",
+            Post post2 = new Post(author1, 
+                "Working with the Set Interface in Java",
                 "The Set interface is implemented by several collections in Java bla bla bla",
-                "Development");            
+                Categories.DEVELOPMENT);            
 
-            Post post3 = new Post("Jane Doe", 
-                "Publishing GitLab pipelines",
+            Post post3 = new Post(author2, 
+               "Publishing GitLab Pipelines",
                 "To publish pipelines in GitLab bla bla bla",
-                "DevOps");            
+                Categories.DEVOPS);            
 
-            Post post4 = new Post("Jane Doe", 
-                "Azure Service Bus where to start",
+            Post post4 = new Post(author2, 
+                "Azure Service Bus Where to Start",
                 "First, it's necessary to understand that bla bla bla",
-                "DevOps");
+                Categories.DEVOPS);
 
-            Post post5 = new Post("Jane Doe", 
-                "TDD made simple",
+            Post post5 = new Post(author2, 
+                "TDD Made Simple",
                 "The TDD software development process bla bla bla",
-                "Development");       
+                Categories.DEVELOPMENT);       
+
+            Post post6 = new Post(author3,
+                "Hadoop: The Tool That Will Ease Your Data Processing",
+                "Hadoop is a tool bla bla bla",
+                Categories.DATA_SCIENCE);
+
+            Post post7 = new Post(author3,
+                "Hadoop: The Tool That Will Ease Your Data Processing",
+                "The Hadoop tool bla bla bla",
+                Categories.DATA_SCIENCE);
 
             blog1.addPost(post1);
             blog1.addPost(post2);
             blog1.addPost(post3);
             blog1.addPost(post4);
             blog1.addPost(post5);
+            blog1.addPost(post6);
 
-            Set<String> author = blog1.getAllAuthors();
+            try {
+                blog1.addPost(post7);
+            } catch(Exception ex) {
+                System.out.println(ex.getMessage());
+            }
 
-            System.out.println(author);
+            Set<Author> authors = blog1.getAllAuthors();
 
-            Map<String, Integer> counting = blog1.getCountByCategory();
+            System.out.println(authors);
+
+            Map<Categories, Integer> counting = blog1.getCountByCategory();
 
             System.out.println(counting);
+
+            Set<Post> postsDevOps = blog1.getPostsByCategory(Categories.DEVOPS);
+
+            System.out.println(postsDevOps);
+
+            Set<Post> postsDev = blog1.getPostsByCategory(Categories.DESENVOLVIMENTO);
+
+            System.out.println(postsDev); 
+
+            Set<Post> postsData = blog1.getPostsByCategory(Categories.DATA_SCIENCE);
+
+            System.out.println(postsData); 
+
+            Set<Post> postsJaneDoe = blog1.getPostsByAuthor(new Author("Jane", "Doe"));
+
+            System.out.println(postsJaneDoe);      
+
+            Map<Categories, Set<Post>> postsByCategories = blog1.getAllPostsByCategories();
+
+            System.out.println(postsByCategories);
+
+            Map<Author, Set<Post>> postsByAuthor = blog1.getAllPostsByAuthor();
+
+            System.out.println(postsByAuthor);        
         }
     }
