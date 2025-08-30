8f6e05adf977e6e37e96b43b900cf5ff42cf1230
f <fidanalizada06@gmail.com>
2025-08-30 10:55:01 +0000

    .

--- Generics_Exercises/warehouse/Electronic.java
+++ Generics_Exercises/warehouse/Electronic.java
@@ -11,3 +11,14 @@ public class Electronic {
     public String getDescription() {
         return description;
     }
+
+    public double getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        // force comma for decimal separator
+        return String.format(Locale.GERMANY, "[%s] $ %.6f", description, value);
+    }
+}
