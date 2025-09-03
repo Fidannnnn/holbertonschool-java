d1b8c6685536a7fd58cdf4fbf2efb7cc17bb6a11
f <fidanalizada06@gmail.com>
2025-09-03 04:14:27 +0000

    .

--- Lambda_Exercises/search_products/ProductSearch.java
+++ Lambda_Exercises/search_products/ProductSearch.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProductSearch {
-    public static List<Product> filter(List<Product> products, FilterCriteria criteria) {
+    public static List <Product> filter(List<Product> products, FilterCriteria criteria) {
         List<Product> result = new ArrayList<>();
         for (Product p : products) {
             if (criteria.test(p)) {
