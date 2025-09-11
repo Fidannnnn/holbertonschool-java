c7697bc92ffeb2a9790d3f86bbce0b2c3f08cb6c
f <fidanalizada06@gmail.com>
2025-09-11 07:28:09 +0000

    .

--- 0x01/email/src/EmailTest.java
+++ 0x01/email/src/EmailTest.java
@@ -10,16 +10,12 @@ class EmailTest {
 
     @Test
     void test_email_without_at_symbol() {
-        assertTrue(!Person.isEmailValid("email_testdomain.com"));
-    }
-
-    @Test
-    void testar_email_sem_arroba() {
-        assertTrue(!Person.isEmailValid("email_testdomain.com"));
+        assertFalse(Person.isEmailValid("email_testdomain.com"));
     }
 
     @Test
     void test_email_over_50_characters() {
+        // length > 50 → invalid
         String tooLong = "email_test_very_long_should_not_be_valid@domain.com";
         assertEquals(false, Person.isEmailValid(tooLong));
     }
